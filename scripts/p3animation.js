93c8ea47107da3b17408739b5d747fc8a9c41a19
zydezu <kinomaxxing@gmail.com>
2025-09-26 20:56:37 +0100

    FIX: preload images

--- scripts/p3animation.js
+++ scripts/p3animation.js
@@ -2,12 +2,22 @@ document.addEventListener("DOMContentLoaded", () => {
     const intro = document.getElementById("bg-video-intro");
     const loop = document.getElementById("bg-video-loop");
 
-    setTimeout(() => {
-        document.getElementById("blurred-glass-pane").classList.add("blurred");
-    }, 1500);
+    const preloadImages = [intro.src, loop.src].map(src => {
+        return new Promise(resolve => {
+            const img = new Image();
+            img.src = src;
+            img.onload = resolve;
+        });
+    });
 
-    setTimeout(() => {
-        intro.style.opacity = 0;
-        loop.style.opacity = 1;
-    }, 5033);
+    Promise.all(preloadImages).then(() => {
+        setTimeout(() => {
+            document.getElementById("blurred-glass-pane").classList.add("blurred");
+        }, 1500);
+
+        setTimeout(() => {
+            intro.style.opacity = 0;
+            loop.style.opacity = 1;
+        }, 5033);
+    });
 });
